53eb48fcfb3164b5350c0e64206a79270c641dea
NataliaDrause <nataliadrause@gmail.com>
2023-01-31 12:27:29 +0200

    fix button color for compatibility with different themes

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'abd2ec434071a993cb4d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'd65e6d580e5f67397193');
